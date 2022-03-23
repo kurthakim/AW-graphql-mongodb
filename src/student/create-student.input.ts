1b76b2d0fbc1f53e0dcc07556c8786955a8fc060
Kurt Hakim <kurthakim@gmail.com>
2022-03-23 23:56:25 +0700

    finished code

---
+++ src/student/create-student.input.ts
@@ -0,0 +1,15 @@
+import { Field, InputType } from '@nestjs/graphql';
+import { IsString, MinLength } from 'class-validator';
+
+@InputType()
+export class CreateStudentInput {
+  @MinLength(1)
+  @IsString()
+  @Field()
+  firstName: string;
+
+  @MinLength(1)
+  @IsString()
+  @Field()
+  lastName: string;
+}
